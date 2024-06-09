f902b60be54123983c4307bf3b84f24be3d800b8
Muhyideen <muhyideeeen@gmail.com>
2024-06-09 23:28:03 +0100

    add all files

---
+++ src/main/java/com/interswitch/hello_interswitch/HelloInterswitchApplication.java
@@ -0,0 +1,13 @@
+package com.interswitch.hello_interswitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloInterswitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloInterswitchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interswitch/hello_interswitch/HelloInterswitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interswitch.hello_interswitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloInterswitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
